6068fab21172f1a44ec05db2b7f0eddf9f4c0a55
himchan92 <omea92@naver.com>
2025-12-16 13:11:18 +0900

    할인정책 AppConfig 활용 실습

--- src/main/java/hello/core/MemerApp.java
+++ src/main/java/hello/core/MemerApp.java
@@ -10,7 +10,7 @@ public class MemerApp {
     public static void main(String[] args) {
 
         //AppConfig 설정정보가져오기
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(MemerApp.class);
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
         MemberService memberService = applicationContext.getBean("memberService", MemberService.class);//Bean 명시된 파라미터 객체 찾기
 
         Member member = new Member(1L, "memberA", Grade.VIP);
